6d9ebe01351dd0dfac7fefdba6e100fb52eb1fee
sebastianjmacharette <sebastian.j.macharette@gmail.com>
2025-04-25 12:16:13 -0300

    One

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='bg-gray-200 h-screen flex justify-center items-center'>
+      Home Page
+    </div>
+  );
+}
+
+export default Home;
