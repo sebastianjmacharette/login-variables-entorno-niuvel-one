f35c229213970764c6e68de6747987e2ea0f0ada
sebastianjmacharette <sebastian.j.macharette@gmail.com>
2025-04-26 18:49:47 -0300

    subida

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className='bg-gray-200 h-screen flex justify-center items-center'>
-      Home Page
+    <div className='bg-gray-800 h-screen flex justify-center items-center'>
+      <h1  className='text-4xl text-white m-10'>Home Page</h1>
     </div>
   );
 }
